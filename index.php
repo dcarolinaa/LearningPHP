b7ecc98e752996a59485ac4b82bbeeb1baf9711c
Carolina Diaz <c.diaz.arredondo@gmail.com>
2021-02-06 20:21:21 -0600

    Frutitas primera versión

--- index.php
+++ index.php
@@ -1,48 +1,23 @@
 <?php
-header('Content-type:text/plain');
 
-$mensaje = "Hola"; /*"" permite concatenación de variables :\ */
-$mensaje2 = 'Mundo!'; /*De preferencia utilizar '' para las cadenas*/
+$frutitas = 'Frambuesa con gusanos, Mango, Fresa, Manzana, Uva';
 
-echo $mensaje . ' ' . $mensaje2 . PHP_EOL;//"." es caracter de concatenación (une cadenas)
-//Se pueden omitir los espacios pero esto hace más legible el código
+$frutitasArr = explode(', ',$frutitas);
 
-$fulano = 'Fulanito';
+?>
+<html>
+<header> 
+</header>
+<body>
+    <h1>Frutitas</h1>
+    <ul> 
+        <?php
+        foreach($frutitasArr as $frutita){            
+            //echo sprintf('<li> %s </li>',$frutita);
+            printf('<li> %s </li>',$frutita);
+        }
+        ?>
+    </ul>
+</body>
+</html>
 
-$mensaje = "Hola $fulano \n";
-echo $mensaje;
-
-echo 'Hola $fulano' . PHP_EOL;
-
-echo sprintf('Hola %s%s %1$s %2$s %1$s %2$s %3$s', $fulano, "!","\n"); 
-//SIEMPRE QUE SEA POSIBLE UTILIZAR ESTA FUNCION (Es la más segura)
-
-//Heredoc
-$str = <<<TEXT
-Ejemplo de una cadena 
-expandida en varias 'líneas
-y admite los caracteres " y ' :3
-TEXT;
-//distintivo para lo que contiene
-
-echo $str;
-
-$mensajeh = 'vamo a ver qué hace el chop xDD        ';
-
-echo $mensajeh;
-echo 'holi1';
-echo chop($mensajeh);
-echo "holi2 \n";
-
-//chop = rtrim quita los ultimos espacios de la cadena
-
-echo chr(64);
-
-/*
-for( $i = 33; $i <= 126; $i++){
-    echo chr($i) . "\n";
-}
-*/
-
-
-echo chunk_split($str, 4,"@");
