6f8a7ca21cc65dc30f37507c94cbf36a15a13287
Carolina Diaz <c.diaz.arredondo@gmail.com>
2021-03-27 18:29:45 -0600

    Refactoring Functional CountryTest

--- code/tests/functional/CountryTest.php
+++ code/tests/functional/CountryTest.php
@@ -8,6 +8,15 @@ use PHPUnit\Framework\TestCase;
 use GuzzleHttp\Client;
 
 class CountryTest extends TestCase{
+    private $getURL;
+    private $countriesRepository;
+    private $client;
+
+    public function setUp() : void{
+        $this->getURL = new GetURL;
+        $this->countriesRepository = new CountriesRepository();
+        $this->client = new Client();
+    }
 
     public function pages  (){
         return [
@@ -21,56 +30,32 @@ class CountryTest extends TestCase{
      * @dataProvider pages
      */
     public function testPages($page){
-        $getURL = new GetURL();
-            $url = $getURL($page,'Countries', [], false);
-            $client = new Client();
-            $response = $client->request('GET', $url);
-            $this->assertSame(200, $response->getStatusCode());
+        $url = $this->getURL->__invoke($page, 'Countries', [], false);        
+        $response = $this->client->request('GET', $url);
+        $this->assertSame(200, $response->getStatusCode());
     }
 
-    // public function testIndex(){
-    //     $getURL = new GetURL();
-    //     $url = $getURL('index','Countries', [], false);
-    //     $client = new Client();
-    //     $response = $client->request('GET', $url);
-    //     $this->assertSame(200, $response->getStatusCode());
-    // }
-
     public function testStore(){
-        $getURL = new GetURL();
-        $url = $getURL('store','Countries', [], false);
-        $indexURL = $getURL('index','Countries');
-        $client = new Client();
-        $response = $client->request('POST', $url, [
+        $url = $this->getURL->__invoke('store', 'Countries', [], false);        
+        $response = $this->client->request('POST', $url, [
             'form_params' => [
                 'name' => 'country functional',
                 'code' => 'cf'                
             ],
             'allow_redirects' => false
         ]);
-        // $this->assertSame()
-        // var_dump($response->getHeaders());
-        // echo $response->getHeader('Location');
-        $headerLocation = $response->getHeader('Location');
-        $this->assertCount(1, $headerLocation);
-        $this->assertSame($indexURL, $headerLocation[0]);
 
-        //var_dump($response->getBody()->__toString());
-        $this->assertSame(302, $response->getStatusCode());
+        $this->assertStatusHeader($response);
     }
 
-    public function testUpdate(){
-        $getURL = new GetURL();
-        $countryRepository = new CountriesRepository();
-        $indexURL = $getURL('index','Countries');
-        $client = new Client();
+    public function testUpdate(){        
         $newName = 'country functional edited';
         $newCode = 'CFE';
 
-        //update should be by PUT method
-        $country = $countryRepository->getLast();
-        $editURL = $getURL('update','Countries',[], false);
-        $response = $client->request('POST', $editURL, [
+		$country = $this->countriesRepository->getLast();
+		$editURL = $this->getURL->__invoke('update','Countries',[], false);
+		
+        $response = $this->client->request('POST', $editURL, [
             'form_params' => [
                 'name' => $newName,
                 'code' => $newCode,
@@ -78,37 +63,32 @@ class CountryTest extends TestCase{
             ],
             'allow_redirects' => false
         ]);
-        // var_dump($response->getBody()->__toString());
+        
+        $editedCountry = $this->countriesRepository->getById($country->getId());
 
-        $headerLocation = $response->getHeader('Location');
-        $this->assertCount(1, $headerLocation);
-        $this->assertSame($indexURL, $headerLocation[0]);
-        $this->assertSame(302, $response->getStatusCode());
-
-        $editedCountry = $countryRepository->getById($country->getId());
-
+        $this->assertStatusHeader($response);
+        $this->assertSame($editedCountry->getName(), $newName);
+        $this->assertSame($editedCountry->getCode(), $newCode);
     }
 
-    public function testDelete(){
-        $getURL = new GetURL();
-        $countryRepository = new CountriesRepository();        
-        $client = new Client();
-
-        $country = $countryRepository->getLast();
-        //delete should be by DELETE method
-        $deleteURL = $getURL('delete','Countries',['id' => $country->getId()],false);
-        $response = $client->request('GET', $deleteURL, [            
+    public function testDelete(){        
+        $country = $this->countriesRepository->getLast();
+        //delete should be by DELETE method        
+		$deleteURL = $this->getURL->__invoke('delete','Countries',['id' => $country->getId()],false);
+        $response = $this->client->request('GET', $deleteURL, [            
             'allow_redirects' => false
         ]);
 
-        $indexURL = $getURL('index','Countries');
-        $headerLocation = $response->getHeader('Location');
-        $this->assertCount(1, $headerLocation);
-        $this->assertSame($indexURL, $headerLocation[0]);
-        $this->assertSame(302, $response->getStatusCode());
-
-        $dbCountry = $countryRepository->getById($country->getId());
+		$this->assertStatusHeader($response);
+        $dbCountry = $this->countriesRepository->getById($country->getId());
         $this->assertNull($dbCountry);
-
     }
+
+    private function assertStatusHeader($response){			
+		$indexURL = $this->getURL->__invoke('index','Countries');
+		$headerLocation = $response->getHeader('Location');
+		$this->assertCount(1, $headerLocation);
+		$this->assertSame($indexURL, $headerLocation[0]);
+		$this->assertSame(302, $response->getStatusCode());	
+	}
 }
